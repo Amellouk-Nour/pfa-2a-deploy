e732d5d308f3d4f5a3b9ea44f2e883909277ab1a
Amellouk-Nour <nour.nouramellouk@gmail.com>
2024-06-05 17:36:39 +0100

    try gitLab miroring

--- deploy.py
+++ deploy.py
@@ -18,7 +18,7 @@ def about_page():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # predicting 
+    # predicting the technique
     input_text = request.form['inputText']
     print(input_text)
     text = [input_text]
